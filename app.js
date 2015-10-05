af9f94b3c769c9698efde133fe5e802483a08310
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-10-05 10:08:18 -0700

    setting up app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,36 +28,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+switch(app.get('env')){
+    case 'development':
+        mongoose.connect(dbConfig.mongo.dev.conn, dbConfig.mongo.options);
+        break;
+    case 'production':
+        mongoose.connect(dbConfig.mongo.prod.conn, dbConfig.mongo.options);
+        break;
+    default:
+        throw new Error('Unknown execution environment: ' + app.get('env'));
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
+
+app.listen(8080);
+console.log('server starting...go to localhost:8080');
